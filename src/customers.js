12b87e2f211fb8436ed32abc26155472fb25ad31
Marina2022 <marusiiiia@yandex.ru>
2024-03-23 14:36:42 +0300

    first slide

--- src/customers.js
+++ src/customers.js
@@ -1,36 +1,36 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import {Navigation, Pagination} from 'swiper/modules';
 // import Swiper and modules styles
 
 
 // init Swiper:
 const swiper = new Swiper('.swiper', {
-  // configure Swiper to use modules
   modules: [Navigation],
   direction: 'horizontal',
-  // loop: true,
 
   slidesPerView: 1,
   spaceBetween: 30,
   centeredSlides: true,
 
-  breakpoints: {
-    // mobile + tablet - 320-990
-    // 320: {
-    //   slidesPerView: 1
-    // },
-    // desktop >= 991
-    768: {
-      slidesPerView: 1
-    }
-  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+});
 
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
+swiper.on('slideChange', function (e) {
+
+  swiper.slides.forEach(slide=>{
+    const el = slide.querySelector('video')
+      if (el){
+        el.pause()
+      }
+  })
+
+  const el = swiper.slides[swiper.activeIndex]
+
+
+
+  el.querySelector('video').play()
+
 });
